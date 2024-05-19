8ea531d7d99e105a7e56ef28b2e78d43b87a39e8
newstar1234 <138200800+newstar1234@users.noreply.github.com>
2024-05-19 13:13:10 +0900

    [level 0] Title: A로 B 만들기, Time: 0.36 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -8,9 +8,17 @@ class Solution {
         Arrays.sort(be);
         Arrays.sort(af);
         
-        if(Arrays.equals(be, af)) {
-            answer = 1;
-        } else answer = 0;
+        //1
+        answer = Arrays.equals(be, af) ? 1 : 0;
+        
+        //2
+        // String s1 = new String(be);
+        // String s2 = new String(af);
+        // answer = s1.equals(s2) ? 1 : 0;
+        
+        // if(Arrays.equals(be, af)) {
+        //     answer = 1;
+        // } else answer = 0;
         
         return answer;
     }
