e068044110fca679dda1fa534d55b1573587d2a0
newstar1234 <138200800+newstar1234@users.noreply.github.com>
2024-06-02 09:56:54 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.38 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 나누어 떨어지는 숫자 배열 - 12910 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910) 
 
 ### 성능 요약
 
-메모리: 83.2 MB, 시간: 3.08 ms
+메모리: 74.3 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 25일 15:46:49
+2024년 06월 02일 09:56:59
 
 ### 문제 설명
 
--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -3,16 +3,36 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int divisor) {
         
-        int [] resultArray = 
-            Arrays.stream(arr).
-            filter(num -> num % divisor == 0).
-            sorted().
-            toArray();
+//         int [] resultArray = 
+//             Arrays.stream(arr).
+//             filter(num -> num % divisor == 0).
+//             sorted().
+//             toArray();
         
-        if(resultArray.length == 0) {
-            return new int[]{-1};
+//         if(resultArray.length == 0) {
+//             return new int[]{-1};
+//         }
+        
+//         return resultArray;
+        
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++) {
+           if(arr[i] % divisor == 0) 
+               list.add(arr[i]);            
         }
+        answer = new int[list.size()];
+        if(list.size() == 0) {
+            answer = new int[1];
+            answer[0] = -1;
+            return answer;
+        }
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        Arrays.sort(answer);
+
+        return answer;
         
-        return resultArray;
     }
 }
