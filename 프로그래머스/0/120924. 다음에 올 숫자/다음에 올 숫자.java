3012bf957954c99075d7de044d13a6f5491da6ea
newstar1234 <138200800+newstar1234@users.noreply.github.com>
2024-06-03 15:28:26 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        for(int i = 0; i < common.length; i++) {
+        
             if(common[1] - common[0] == common[2] - common[1]) {
                 // 등차수열일때
                 answer = common[1] - common[0] + common[common.length-1]; 
@@ -10,7 +10,7 @@ class Solution {
                 // 등비수열일때
                 answer = common[1] / common[0] * common[common.length-1];
             }
-        }
+        
         return answer;
     }
 }
